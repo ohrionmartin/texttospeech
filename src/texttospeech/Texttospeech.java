eea8d5019e62fb54da095bad83d12b760ecdcbcd
OH Martin <38046346+orion-martin@users.noreply.github.com>
2018-04-17 02:08:13 +0200

    A text to speech app
    converts text from computer keyboard to speech in many different languages

---
+++ src/texttospeech/Texttospeech.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package texttospeech;
+
+/**
+ *
+ * @author ORION
+ */
+public class Texttospeech {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainFrame().setVisible(true);
+            }
+        });
+    }
+    
+}
